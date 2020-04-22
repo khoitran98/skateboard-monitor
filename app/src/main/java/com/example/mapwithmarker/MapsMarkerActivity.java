77d98d908762301b3515ea46ffeb4bc0d491f7db
Khoi Tran <trandangkhoi1998@gmail.com>
2020-04-22 01:04:04 -0500

    First commit

---
+++ app/src/main/java/com/example/mapwithmarker/MapsMarkerActivity.java
@@ -0,0 +1,56 @@
+package com.example.mapwithmarker;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import android.view.View;
+
+/**
+ * An activity that displays a Google map with a marker (pin) to indicate a particular location.
+ */
+public class MapsMarkerActivity extends AppCompatActivity
+        implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Retrieve the content view that renders the map.
+        setContentView(R.layout.activity_maps);
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map when it's available.
+     * The API invokes this callback when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user receives a prompt to install
+     * Play services inside the SupportMapFragment. The API invokes this method after the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+//        LatLng sydney = new LatLng(32.983716, -96.752076);
+//        LatLng sydney2 = new LatLng(32.986716, -96.756076);
+//        googleMap.addMarker(new MarkerOptions().position(sydney)
+//                .title("Marker"));
+//        googleMap.addMarker(new MarkerOptions().position(sydney2)
+//                .title("Markr"));
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13.0f));
+    }
+
+
+}
